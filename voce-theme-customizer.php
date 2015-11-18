18da77524b0fc3e26ffd3ff208d79f9356cbd470
Gary Smirny <banderon@gmail.com>
2015-11-18 14:25:56 -0500

    bumped version #

--- voce-theme-customizer.php
+++ voce-theme-customizer.php
@@ -3,7 +3,7 @@
   Plugin Name: Voce Theme Customizer
   Plugin URI: http://plugins.voceconnect.com
   Description: Theme Customizer
-  Version: 1.2
+  Version: 1.2.1
   Author: banderon, csloisel, jeffstieler, kevinlangleyjr
   License: GPL2
  */
